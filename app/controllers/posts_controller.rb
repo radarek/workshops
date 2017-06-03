0c35464e8ee4e1b19a6f3a8a925dbc48284585aa
Radosław Bułat <radek.bulat@gmail.com>
2017-06-03 14:19:25 +0200

    Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :fetch_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order("created_at desc")
